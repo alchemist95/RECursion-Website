26a581bc1c90f13eeb7a727b62eea133a27193c6
anand13696 <anand.13696@gmail.com>
2017-10-12 00:22:34 +0530

    Fixed status for update profile

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 	$("#nickname").keyup(function(){
 			nickname = $(this).val();
 			if(/^[a-zA-Z0-9_]*$/.test(nickname) == false || nickname.length < 4) {
-    			$('#status').hide().html('<p>Nickname should be of length more than 4</p>').fadeIn("slow");
+    			$('#status').hide().html('Username (Minimum 4 letters)').fadeIn("slow");
     			$(':input[type="submit"]').prop('disabled', true);
 				return;
 			}
@@ -26,10 +26,10 @@ $(document).ready(function() {
 				success: function(result){
 					if(result.status==='GoodToGo'){
 						$(':input[type="submit"]').prop('disabled', false);
-    					$('#status').hide().html('<p>Available</p>').fadeIn("slow");
+    					$('#status').hide().html('Username (Available)').fadeIn("slow");
 					}
 					else{
-    					$('#status').hide().html('<p>Choose Another</p>').fadeIn("slow");
+    					$('#status').hide().html('Username (Not Available)').fadeIn("slow");
 						$(':input[type="submit"]').prop('disabled', true);
 					}
 				},
