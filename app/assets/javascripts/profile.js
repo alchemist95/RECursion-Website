a6024a35889c7e5da3614da6fe79afbfaed8fd64
anand13696 <anand.13696@gmail.com>
2017-10-21 00:38:25 +0530

    Profile Page cheanges

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -16,7 +16,7 @@
 	          var newHTML = [];
 	          for (var i = 0; i < result.users.length; i++) {
 	          	console.log(result.users[i][0]);
-	            newHTML.push('<a href="/users/profile/'+result.users[i][0]+'"><table class="highlight"><td><span style="line-height: 30px">'+result.users[i][0]+'(<i style="font-size: 10px">'+ result.users[i][1] + '</i>)</span><span style="float:right"><img class="circle" src="' + result.users[i][2] + '" alt="Photo" width="40" height="40" ></span></td></table></% end /%>');
+	            newHTML.push('<a href="/users/profile/'+result.users[i][0]+'"><table class="highlight"><td><span style="line-height: 30px">'+result.users[i][0]+'<i style="font-size: 10px; color: #bdbdbd">('+ result.users[i][1] + ')</i></span><span style="float:right"><img class="circle" src="' + result.users[i][2] + '" alt="Photo" width="40" height="40" ></span></td></table></% end /%>');
 	          }
 	          $('.search_results').html(newHTML);
 	        },
