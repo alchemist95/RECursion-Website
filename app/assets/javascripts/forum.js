fc7ffdd27676fb080e3393339054ec3999cdeb2c
anand13696 <anand.13696@gmail.com>
2017-10-21 13:31:16 +0530

    Bugfix: Tags search

--- app/assets/javascripts/forum.js
+++ app/assets/javascripts/forum.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
         success: function(result){
           var newHTML = [];
           for (var i = 0; i < result.tags.length; i++) {
-            newHTML.push('<h6><a href="/askrec/questions/tags/' + result.tags[i][0] + '"><table class="highlight"><td><span>'+ result.tags[i][0] + '</a><span class="new badge" data-badge-caption="">' + result.tags[i][1] + '</span></td></table></h6><br />');
+            newHTML.push('<a href="/askrec/questions/tags/' + result.tags[i][0] + '"><table class="highlight"><td><span>'+ result.tags[i][0] + '</a><span class="new badge" data-badge-caption="">' + result.tags[i][1] + '</span></td></table>');
           }
           $('.search_results').html(newHTML);
         },
