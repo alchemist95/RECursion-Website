8188528ddcb82b47d5150850db9df98d2f03b212
anand13696 <anand.13696@gmail.com>
2018-01-13 14:32:33 +0530

    Bugfix

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/home/deploy/RECursion-Website"
 set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/application.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "bin", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
